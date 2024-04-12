0babb78eadacff52e925469f64d2532252d7f27f
missosmal <fadesteam8@gmail.com>
2024-04-12 14:23:17 +0500

    Разработанные модели.Graf

---
+++ Models/Clubs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PcClubs_Graf.Models
+{
+    public class Clubs
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public string Address { get; set; }
+        public string WorkTime{ get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PcClubs_Graf.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public DateTime RentStart { get; set; }
+        public int Duration { get; set; }
+        public int IdClub { get; set; }
+    }
+}
